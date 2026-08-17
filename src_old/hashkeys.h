c7c7f7592c5a7d983046ea7cea5206fca51421fb
Gabrielkaos <gabrielmontes155@gmail.com>
2026-08-17 14:59:18 +0800

    removed src_old

--- src_old/hashkeys.h
+++
@@ -1,9 +0,0 @@
-#ifndef HASHKEYS_H
-#define HASHKEYS_H
-
-//hashkeys.c
-extern U64 GeneratePosKey(const S_BOARD *pos);
-//extern U64 GeneratePawnPosKey(const S_BOARD *pos);
-extern U64 GeneratePKHash(const S_BOARD *pos);
-
-#endif // HASHKEYS_H
