2175411ed73f6542f046bd05b33a090b15d39f35
Gabrielkaos <gabrielmontes155@gmail.com>
2026-08-23 08:30:56 +0800

    minor

--- tools/datagen.py
+++ tools/datagen.py
@@ -29,7 +29,7 @@ ENGINE = os.environ.get(
 )
 NODES = int(sys.argv[1]) if len(sys.argv) > 1 else 5000
 GAMES = int(sys.argv[2]) if len(sys.argv) > 2 else 10000
-THREADS = int(sys.argv[3]) if len(sys.argv) > 3 else os.cpu_count() or 4
+THREADS = int(sys.argv[3]) if len(sys.argv) > 3 else 5
 OUT = sys.argv[4] if len(sys.argv) > 4 else "dataset.epd"
 BOOK_FILE = sys.argv[5] if len(sys.argv) > 5 else "book.epd"
 POS_CAP = int(sys.argv[6]) if len(sys.argv) > 6 else 0
